2d18abb647780402c8013c3aaa6c626d8870d20b
jimmy <675740051@qq.com>
2021-02-25 22:49:19 +0800

    init commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    themeConfig: {
+        navbar: true,
+        logo: '/assets/img/logo.jpg',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            {
+                text: 'External',
+                ariaLabel: 'External Menu',
+                items: [
+                    { text: 'Chinese', link: '/guide/' },
+                    { text: 'Japanese', link: '/language/japanese/' }
+                ]
+            },
+        ],
+        // sidebar: [
+        //     '/',
+        //     '/page-a',
+        //     ['/page-b', 'Explicit link text']
+        // ]
+        // sidebar: 'auto'
+    }
+}
