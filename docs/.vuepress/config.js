675f313e28ac3669935823ed2025f1843632751f
agentleman <675740051@qq.com>
2021-02-26 13:25:59 +0800

    home pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,25 @@
 module.exports = {
-    themeConfig: {
-        navbar: true,
-        base:"/vue-press/",
-        logo: '/assets/img/logo.jpg',
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            {
-                text: 'External',
-                ariaLabel: 'External Menu',
-                items: [
-                    { text: 'Chinese', link: '/guide/' },
-                    { text: 'Japanese', link: '/language/japanese/' }
-                ]
-            },
+  themeConfig: {
+    navbar: true,
+    base: "/vue-press/",
+    logo: "/assets/img/logo.jpg",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      {
+        text: "External",
+        ariaLabel: "External Menu",
+        items: [
+          { text: "Chinese", link: "/guide/" },
+          { text: "Japanese", link: "/language/japanese/" },
         ],
-        // sidebar: [
-        //     '/',
-        //     '/page-a',
-        //     ['/page-b', 'Explicit link text']
-        // ]
-        // sidebar: 'auto'
-    }
-}
+      },
+    ],
+    // sidebar: [
+    //     '/',
+    //     '/page-a',
+    //     ['/page-b', 'Explicit link text']
+    // ]
+    // sidebar: 'auto'
+  },
+};
